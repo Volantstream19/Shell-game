e36cf43380828ee946b4f3f58091706f76a7e2c6
King Kong <king-kong@gmail.com>
2022-09-12 18:20:22 -0700

    fixed console errors

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* Imports */
-    import { getRandomItem }'./utils.js';
+// import { getRandomItem }'./utils.js';
 
 /* State */
 let gameState = 'guess';
@@ -16,27 +16,38 @@ function loadPage() {
     displayScoreboard();
 }
 
-
 /* Components */
 
-const totalDisplay = document.getElementById('total-Display');
-const winsDisplay = document.getElementById('wins-Display');
-const lossesDisplay = document.getElementById('losses-Display');
+const totalDisplay = document.getElementById('total-display');
+const winsDisplay = document.getElementById('wins-display');
+const lossesDisplay = document.getElementById('losses-display');
 
 function displayScoreboard() {
     totalDisplay.textContent = total;
     winsDisplay.textContent = wins;
-    lossesDisplay.textContent = losses;
-
-
-
-
-
-// get DOM
-
+    lossesDisplay.textContent = total - wins;
 }
-// display
+const results = document.getElementById('results');
 
+const pearl1 = document.getElementById('pearl-1');
+const pearl2 = document.getElementById('pearl-2');
+const pearl3 = document.getElementById('pearl-3');
+
+const shell1 = document.getElementById('shell-1');
+const shell2 = document.getElementById('shell-2');
+const shell3 = document.getElementById('shell-3');
+
+//display
+function displayShells() {
+    if (gameState === 'guess') {
+        results.classList.add('hidden');
+        pearl1.classList.add('hidden');
+        pearl2.classList.add('hidden');
+        pearl3.classList.add('hidden');
+        shell1.classList.remove('reveal');
+    } else {
+        results.classList.remove('hidden');
+    }
 }
 // event listeners
 
