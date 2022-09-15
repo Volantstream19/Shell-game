0a770ed343e96c17211212e3410affa5d3659f54
King Kong <king-kong@gmail.com>
2022-09-14 17:53:29 -0700

    finished shell game

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
-/* Imports */
-// import { getRandomItem }'./utils.js';
+import { getRandomItem } from './utils.js';
 
-/* State */
 let gameState = 'guess';
-let guess = '';
-let result = '';
-let reveal = '';
-
 let total = 0;
 let wins = 0;
+let losses = 0;
 
-/* Actions */
-function loadPage() {
-    displayShells();
-    displayScoreboard();
-}
-
-/* Components */
-
-const totalDisplay = document.getElementById('total-display');
-const winsDisplay = document.getElementById('wins-display');
-const lossesDisplay = document.getElementById('losses-display');
-
-function displayScoreboard() {
-    totalDisplay.textContent = total;
-    winsDisplay.textContent = wins;
-    lossesDisplay.textContent = total - wins;
-}
 const results = document.getElementById('results');
 
 const pearl1 = document.getElementById('pearl-1');
@@ -37,19 +15,74 @@ const shell1 = document.getElementById('shell-1');
 const shell2 = document.getElementById('shell-2');
 const shell3 = document.getElementById('shell-3');
 
-//display
+const totalDisplay = document.getElementById('total-display');
+const lossesDisplay = document.getElementById('losses-display');
+const winsDisplay = document.getElementById('wins-display');
+
+const guesses = ['shellOne', 'shellTwo', 'shellThree'];
+
+const guessOne = document.getElementById('guess-1');
+const guessTwo = document.getElementById('guess-2');
+const guessThree = document.getElementById('guess-3');
+const guessContainer = document.getElementById('guesses');
+const playAgainButton = document.getElementById('play-again-button');
+let correctPosition = getRandomItem(guesses);
+
+function loadPage() {
+    displayShells();
+}
+
+function handleGuess(userGuess) {
+    total++;
+    totalDisplay.textContent = total;
+    if (userGuess === correctPosition) {
+        wins++;
+        winsDisplay.textContent = wins;
+    } else {
+        losses++;
+        lossesDisplay.textContent = losses;
+    }
+
+    if (correctPosition === 'shellOne') {
+        pearl1.classList.remove('hidden');
+        shell1.classList.add('reveal');
+    }
+    if (correctPosition === 'shellTwo') {
+        pearl2.classList.remove('hidden');
+        shell2.classList.add('reveal');
+    }
+    if (correctPosition === 'shellThree') {
+        pearl3.classList.remove('hidden');
+        shell3.classList.add('reveal');
+    }
+    guessContainer.classList.add('hidden');
+    playAgainButton.classList.remove('hidden');
+    correctPosition = getRandomItem(guesses);
+}
+
 function displayShells() {
     if (gameState === 'guess') {
-        results.classList.add('hidden');
+        results.classList.add('reveal');
         pearl1.classList.add('hidden');
         pearl2.classList.add('hidden');
         pearl3.classList.add('hidden');
         shell1.classList.remove('reveal');
+        shell2.classList.remove('reveal');
+        shell3.classList.remove('reveal');
     } else {
         results.classList.remove('hidden');
     }
 }
-// event listeners
 
-/* Run page load code */
+function playAgain() {
+    displayShells();
+    playAgainButton.classList.add('hidden');
+    guessContainer.classList.remove('hidden');
+}
+
+playAgainButton.addEventListener('click', () => playAgain());
+guessOne.addEventListener('click', () => handleGuess('shellOne'));
+guessTwo.addEventListener('click', () => handleGuess('shellTwo'));
+guessThree.addEventListener('click', () => handleGuess('shellThree'));
+
 loadPage();
